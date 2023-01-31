9e52b100acd33d7b5b28f3bf0d38c6a3e0cc72d3
Tinu George <tgeorge@Tinus-MacBook-Pro.local>
2023-01-31 11:00:19 +0800

    initial commit

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react'
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import {sample} from '../../utils'
+import {WORDS} from '../../data'
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info({answer})
 
 function Game() {
-  return <>Put a game here!</>;
+	const [guess, setGuess] = useState('')
+	return (
+		<form
+			className="guess-input-wrapper"
+			onSubmit={event => {
+				event.preventDefault()
+				setGuess(event.target.elements['guess-input'].value)
+			}}
+		>
+			<label htmlFor="guess-input">Enter guess:</label>
+			<input
+				id="guess-input"
+				type="text"
+				value={guess}
+				onChange={event => setGuess(event.target.value)}
+			/>
+		</form>
+	)
 }
 
-export default Game;
+export default Game
